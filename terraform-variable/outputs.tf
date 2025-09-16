d2f55a2bae2e05478753465e6d4b54f2a2d481e3
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-09-16 18:57:07 +0530

    Update outputs.tf

--- terraform-variable/outputs.tf
+++ terraform-variable/outputs.tf
@@ -1,1 +1,10 @@
 
+output "instance_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = aws_instance.my_ec2.public_ip
+}
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.my_ec2.id
+}
