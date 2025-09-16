50020496817315f5f02d933fa080ffb4910578f3
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-09-16 18:58:15 +0530

    Update variables.tf

--- terraform-variable/variables.tf
+++ terraform-variable/variables.tf
@@ -1,1 +1,15 @@
 
+variable "region" {
+  description = "AWS region to launch resources"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}
