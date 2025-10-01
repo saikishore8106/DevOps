938298e3934bd8e6b006760af4d018036254061e
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-10-01 14:20:23 +0530

    Update app.py

--- Docker-compose/app.py
+++ Docker-compose/app.py
@@ -1,1 +1,23 @@
+from flask import Flask
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Connect to MySQL database
+    connection = mysql.connector.connect(
+        host=os.environ.get("MYSQL_HOST", "db"),
+        user="root",
+        password="root",
+        database="testdb"
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT 'Hello from Flask + MySQL!'")
+    result = cursor.fetchone()
+    return result[0]
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
